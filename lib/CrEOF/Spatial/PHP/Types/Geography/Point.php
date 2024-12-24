0ae7a34cc9bc706b30d61a5dc062f09f05cb5670
Jeremy Antunez <musicjerm@gmail.com>
2024-12-24 12:17:35 -0700

    doctrine compatibility

--- lib/CrEOF/Spatial/PHP/Types/Geography/Point.php
+++ lib/CrEOF/Spatial/PHP/Types/Geography/Point.php
@@ -45,16 +45,6 @@ class Point extends AbstractPoint implements GeographyInterface
      */
     public function setX($x)
     {
-        $parser = new Parser($x);
-
-        try {
-            $x = (float) $parser->parse();
-        } catch (RangeException $e) {
-            throw new InvalidValueException($e->getMessage(), $e->getCode(), $e->getPrevious());
-        } catch (UnexpectedValueException $e) {
-            throw new InvalidValueException($e->getMessage(), $e->getCode(), $e->getPrevious());
-        }
-
         if ($x < -180 || $x > 180) {
             throw new InvalidValueException(sprintf('Invalid longitude value "%s", must be in range -180 to 180.', $x));
         }
@@ -72,16 +62,6 @@ class Point extends AbstractPoint implements GeographyInterface
      */
     public function setY($y)
     {
-        $parser = new Parser($y);
-
-        try {
-            $y = (float) $parser->parse();
-        } catch (RangeException $e) {
-            throw new InvalidValueException($e->getMessage(), $e->getCode(), $e->getPrevious());
-        } catch (UnexpectedValueException $e) {
-            throw new InvalidValueException($e->getMessage(), $e->getCode(), $e->getPrevious());
-        }
-
         if ($y < -90 || $y > 90) {
             throw new InvalidValueException(sprintf('Invalid latitude value "%s", must be in range -90 to 90.', $y));
         }
